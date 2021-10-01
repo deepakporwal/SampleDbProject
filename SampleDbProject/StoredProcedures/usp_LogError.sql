208ab3a35f42eb43644d04fec8116807110c12f9
DESKTOP-N9ORL5H\deepa <er.deepakporwal@gmail.com>
2021-10-01 12:56:23 +0530

    error logging at sql level

---
+++ SampleDbProject/StoredProcedures/usp_LogError.sql
@@ -0,0 +1,28 @@
+﻿CREATE PROCEDURE [dbo].[usp_LogError]
+	
+AS
+	BEGIN
+		SET NOCOUNT ON
+
+		INSERT INTO [dbo].SqlErrorLog 
+		(
+		 [Username],
+		 [ErrorNumber],
+		 [ErrorSeverity],
+		 [ErrorState],
+		 [ErrorProcedure],
+		 [ErrorLine],
+		 [ErrorMessage]
+		)
+		values
+		(
+		Convert(sysname,Current_User),
+		ERROR_NUMBER(),
+		ERROR_SEVERITY(),
+		ERROR_STATE(),
+		ERROR_PROCEDURE(),
+		ERROR_LINE(),
+		ERROR_MESSAGE()
+		)
+	END
+RETURN 
---
+++ SampleDbProject/Tables/SqlErrorLog.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[SqlErrorLog]
+(
+	[ErrorLogId] BIGINT NOT NULL PRIMARY KEY, 
+    [ErrorTime] DATETIME NOT NULL, 
+    [UserName] NVARCHAR(100) NOT NULL, 
+    [ErrorNumber] INT NOT NULL, 
+    [ErrorSeverity] INT NOT NULL, 
+    [ErrorState] INT NOT NULL, 
+    [ErrorProcedure] NVARCHAR(150) NULL, 
+    [ErrorLine] INT NULL, 
+    [ErrorMessage] NVARCHAR(4000) NULL
+)
